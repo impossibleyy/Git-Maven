4a287f30e72745d643216e0043de22a3118fe940
YuSong <YuSong@163.com>
2022-05-07 20:03:08 +0800

    hot-fix first commit

--- src/main/java/com/bjpowernode/GitTest01.java
+++ src/main/java/com/bjpowernode/GitTest01.java
@@ -9,11 +9,17 @@ public class GitTest01 {
     public int sub(int n1,int n2){
         return n1 - n2;
     }
+
+    public int div(int n1,int n2){
+        return n1/n2;
+    }
     public static void main(String[] args) {
         GitTest01 gitTest01 = new GitTest01();
         int add = gitTest01.add(100, 200);
         System.out.println("结果是="+add);
         int sub = gitTest01.sub(100, 200);
         System.out.println("结果是="+sub);
+        int div = gitTest01.div(200, 100);
+        System.out.println("结果是="+div);
     }
 }
